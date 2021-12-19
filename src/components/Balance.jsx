04187a9ca4d8c7d29fb406fc428a6d2a7b912192
Preethi <preethiknot1998@gmail.com>
2021-12-19 12:05:51 +0530

    header and balance component

---
+++ src/components/Balance.jsx
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+class Balance extends React.Component {
+    render() { 
+        return <div>
+            <h4>Your Balance</h4>
+            <h1 id="balance">$0.00</h1>
+        </div>;
+    }
+}
+ 
+export default Balance;
