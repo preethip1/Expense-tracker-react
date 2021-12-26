6cd268703c22da16f355a1e82250d5600a252ab3
Preethi <preethiknot1998@gmail.com>
2021-12-26 19:48:43 +0530

    added transaction component

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import './App.css';
 import Header from './components/Header';
 import Balance from './components/Balance';
-import IncomeExpense from './components/IncomeExpense';
-import TransactionList from './components/TransactionList';
-import AddTransaction from './components/AddTransaction';
+import {IncomeExpense} from './components/IncomeExpense';
+import {TransactionList} from './components/TransactionList';
+import {AddTransaction} from './components/AddTransaction';
 
+import {GlobalProvider} from './context/GlobalState';
 function App() {
   return (
-    <div className="App">
+    <GlobalProvider>
       <Header/>
       <div className='container'>
       <Balance/>
@@ -15,7 +16,7 @@ function App() {
       <TransactionList/>
       <AddTransaction/>
       </div>
-    </div>
+    </GlobalProvider>
   );
 }
 
--- src/components/TransactionList.jsx
+++ src/components/TransactionList.jsx
@@ -1,16 +1,17 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import { Transaction } from './Transaction';
 
-class TransactionList extends React.Component {
-    render() { 
-        return <div>
-            <h3>History</h3>
-            <ul className='list'>
-                <li className='minus'>
-                    Cash <span>-$400</span> <button className='delete-btn'>x</button>
-                </li>
-            </ul>
-        </div>;
-    }
+import { GlobalContext } from '../context/GlobalState';
+
+export const TransactionList = () => {
+  const { transactions } = useContext(GlobalContext);
+
+  return (
+    <>
+      <h3>History</h3>
+      <ul className="list">
+        {transactions.map(transaction => (<Transaction key={transaction.id} transaction={transaction} />))}
+      </ul>
+    </>
+  )
 }
- 
-export default TransactionList;
